0f543c190f4ae831951e4c0b8c9707d015f0a594
sachinkaria <sachin.karia01@gmail.com>
2017-04-01 21:52:03 +0100

    added responsiveness to navbar

--- app/components/NavigationBar.js
+++ app/components/NavigationBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ReactRouter, Link } from 'react-router';
-import { Navbar, Nav, Col } from 'react-bootstrap';
+import { Navbar, Nav, Col, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
 
 export default class NavigationBar extends React.Component {
   render() {
       return (
-          <div className="row">
-              <Navbar bsSize="lg" className="gc-navbar">
-                  <Col xs={4}>
+          <div>
+              <Navbar className="gc-navbar">
+                  <Col>
                       <Navbar.Header>
                           <Navbar.Brand>
                               <Link to="/" className="gc-padding-none">
@@ -17,25 +17,25 @@ export default class NavigationBar extends React.Component {
                           <Navbar.Toggle />
                       </Navbar.Header>
                   </Col>
-                  <Col xs={4} xsOffset={4} className="gc-vertical-center">
-                      <Nav>
-                          <Col xs={4}>
-                              <Link>
-                                  <p>I'm a chef</p>
-                              </Link>
-                          </Col>
-                          <Col xs={4}>
-                              <Link to={'/inbox'}>
-                                  <p>Messages</p>
-                              </Link>
-                          </Col>
-                          <Col xs={4}>
-                              <Link to={'/bookings'}>
-                                  <p>Bookings</p>
-                              </Link>
-                          </Col>
-                      </Nav>
-                  </Col>
+                          <Navbar.Collapse className="gc-navbar-dropdown">
+                              <Nav pullRight>
+                                  <NavItem eventKey={1} href="#">
+                                      <Link to={'/inbox'}>
+                                        <p className="gc-dropdown-item">Messages</p>
+                                      </Link>
+                                  </NavItem>
+                                  <NavItem eventKey={2} href="#">
+                                      <Link>
+                                        <p className="gc-dropdown-item">I'm a chef</p>
+                                      </Link>
+                                  </NavItem>
+                                  <NavItem eventKey={2} href="#">
+                                      <Link to={'/bookings'}>
+                                          <p className="gc-dropdown-item">Bookings</p>
+                                      </Link>
+                                  </NavItem>
+                              </Nav>
+                          </Navbar.Collapse>
               </Navbar>
               {this.props.children}
           </div>

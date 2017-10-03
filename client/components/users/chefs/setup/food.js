afc454aac91d920bbff8ecf7a71a2c6a94ffe611
sachinkaria <sachin.karia01@gmail.com>
2017-10-03 17:39:32 +0100

    created wizard component and refactored food setup step to use wizard component

--- client/components/users/chefs/setup/food.js
+++ client/components/users/chefs/setup/food.js
@@ -5,9 +5,10 @@ import {Field, reduxForm} from 'redux-form';
 import {Col, Panel, Row, Button} from 'react-bootstrap';
 import _ from 'lodash';
 import {updateUser} from '../../../../actions/users';
-import { FOOD_SERVICES, CUISINES } from '../../../../utils/data';
+import {FOOD_SERVICES, CUISINES} from '../../../../utils/data';
 import renderCheckbox from '../../../forms/renderCheckbox';
 import ProgressBar from '../../../progress-bar';
+import Wizard from '../../../wizard';
 
 const URL = '/setup-photos';
 
@@ -61,115 +62,65 @@ class Services extends Component {
     return state && state.indexOf(item) > -1;
   }
 
-  renderAlert() {
-    if (this.props.errorMessage) {
-      return (
-        <div>
-          <span><strong>Error!</strong> {this.props.errorMessage}</span>
-        </div>
-      );
-    }
-  }
-
   render() {
-    const {handleSubmit} = this.props;
+    const { handleSubmit } = this.props;
+    const progress = 0.6;
+    const sideBarHeading = 'services';
+    const sideBarText = 'Your basic information will give everyone an idea of what type of services you are able to offer. You can select more than one category for all of your services.';
+    const onBack = '/setup-services';
+    const onSkip = '/setup-photos';
 
     return (
-      <Row>
-        <div className="gc-progress-bar">
-          <ProgressBar progress={0.6}/>
-        </div>
-        <Col xs={10} xsOffset={1} sm={6} smOffset={1} md={5} mdOffset={2}>
-          <Row>
-            <Col sm={6} smOffset={1}>
-              <Link className="gc-link-default pull-left" to="/setup-services">
-                <Button className="gc-btn gc-btn--white gc-margin-top" bsSize="small" bsStyle="default">
-                  Back
-                </Button>
-              </Link>
-            </Col>
-          </Row>
-          <br />
-          <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-            {this.renderAlert()}
-            <Row>
-              <Col xs={12} sm={11} smOffset={1}>
-                <Panel>
-                  <p className="gc-form-heading">Food Services</p>
-                  <Row>
-                    {
-                      FOOD_SERVICES.map(item => (
-                        <Col sm={6} key={item}>
-                          <Field
-                            checked={this.isChecked(item, this.state.services)}
-                            name={item}
-                            type="checkbox"
-                            component={renderCheckbox}
-                            onChange={e => this.handler(e, 'services')}
-                          />
-                        </Col>
-                      ))
-                    }
-                  </Row>
-                </Panel>
-                <Panel>
-                  <p className="gc-form-heading">Cuisines</p>
-                  <Row>
-                    {
-                      CUISINES.map(item => (
-                        <Col sm={6} key={item}>
-                          <Field
-                            checked={this.isChecked(item, this.state.cuisines)}
-                            name={item}
-                            type="checkbox"
-                            component={renderCheckbox}
-                            onChange={e => this.handler(e, 'cuisines')}
-                          />
-                        </Col>
-                      ))
-                    }
-                  </Row>
-                </Panel>
-              </Col>
-            </Row>
-            <Row>
-              <Col sm={11} smOffset={1}>
-                <Row>
-                  <Col xs={8} xsOffset={2} md={4} mdOffset={4}>
-                    <Button
-                      type="submit"
-                      bsSize="small"
-                      block
-                      className="btn gc-btn gc-btn--orange gc-margin-bottom--xs gc-margin-top">
-                      Next
-                    </Button>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col xs={8} xsOffset={2} md={4} mdOffset={4}>
-                    <Link className="gc-link-default" to="/setup-photos">
-                      <Button className="gc-btn gc-btn--white" bsSize="small" block bsStyle="default">
-                        Skip
-                      </Button>
-                    </Link>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </form>
-        </Col>
-        <Col xsHidden={true} sm={4} md={3}>
-          <Row>
-            <Col sm={11}>
-              <Panel className="gc-panel gc-margin-top">
-                <h3 className="gc-profile-heading-sm">Services</h3>
-                <p className="gc-text">Your basic information will give everyone an idea of what type of services you
-                  are able to offer. You can select more than one category for all of your services.</p>
-              </Panel>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <Wizard
+        onSubmit={handleSubmit(this.handleFormSubmit)}
+        progress={progress}
+        sideBarHeading={sideBarHeading}
+        sideBarText={sideBarText}
+        onBack={onBack}
+        onSkip={onSkip}
+        errorMessage={this.props.errorMessage}
+      >
+        <Row>
+          <Col xs={12} sm={11} smOffset={1}>
+            <Panel>
+              <p className="gc-form-heading">Food Services</p>
+              <Row>
+                {
+                  FOOD_SERVICES.map(item => (
+                    <Col sm={6} key={item}>
+                      <Field
+                        checked={this.isChecked(item, this.state.services)}
+                        name={item}
+                        type="checkbox"
+                        component={renderCheckbox}
+                        onChange={e => this.handler(e, 'services')}
+                      />
+                    </Col>
+                  ))
+                }
+              </Row>
+            </Panel>
+            <Panel>
+              <p className="gc-form-heading">Cuisines</p>
+              <Row>
+                {
+                  CUISINES.map(item => (
+                    <Col sm={6} key={item}>
+                      <Field
+                        checked={this.isChecked(item, this.state.cuisines)}
+                        name={item}
+                        type="checkbox"
+                        component={renderCheckbox}
+                        onChange={e => this.handler(e, 'cuisines')}
+                      />
+                    </Col>
+                  ))
+                }
+              </Row>
+            </Panel>
+          </Col>
+        </Row>
+      </Wizard>
     );
   }
 }
---
+++ client/components/wizard/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { Col, Panel, Row, Button } from 'react-bootstrap';
+import ProgressBar from '../progress-bar';
+
+function Wizard(props) {
+  const children = props.children;
+  return (
+    <Row>
+      <div className="gc-progress-bar">
+        <ProgressBar progress={props.progress}/>
+      </div>
+      <Col xs={10} xsOffset={1} sm={6} smOffset={1} md={5} mdOffset={2}>
+        <Row>
+          <Col sm={6} smOffset={1}>
+            <Link className="gc-link-default pull-left" to={props.onBack}>
+              <Button className="gc-btn gc-btn--white gc-margin-top" bsSize="small" bsStyle="default">
+                Back
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+        <br />
+        <form onSubmit={props.onSubmit}>
+          {children}
+          <Row>
+            <Col sm={11} smOffset={1}>
+              <Row>
+                <Col xs={8} xsOffset={2} md={4} mdOffset={4}>
+                  <p>{props.errorMessage}</p>
+                  <Button
+                    type="submit"
+                    bsSize="small"
+                    block
+                    className="btn gc-btn gc-btn--orange gc-margin-bottom--xs gc-margin-top">
+                    Next
+                  </Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={8} xsOffset={2} md={4} mdOffset={4}>
+                  <Link className="gc-link-default" to={props.onSkip}>
+                    <Button className="gc-btn gc-btn--white" bsSize="small" block bsStyle="default">
+                      Skip
+                    </Button>
+                  </Link>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </form>
+      </Col>
+      <Col xsHidden sm={4} md={3}>
+        <Row>
+          <Col sm={11}>
+            <Panel className="gc-panel gc-margin-top">
+              <h3 className="gc-profile-heading-sm">{props.sideBarHeading}</h3>
+              <p className="gc-text">{props.sideBarText}</p>
+            </Panel>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+}
+
+Wizard.propTypes = {
+  children: React.PropTypes.element.isRequired,
+  progress: React.PropTypes.number,
+  onSubmit: React.PropTypes.func,
+  onSkip: React.PropTypes.string,
+  onBack: React.PropTypes.string,
+  sideBarHeading: React.PropTypes.string,
+  sideBarText: React.PropTypes.string,
+  errorMessage: React.PropTypes.string
+};
+
+Wizard.defaultProps = {
+  children: <div>No view inserted</div>
+};
+
+
+export default Wizard;

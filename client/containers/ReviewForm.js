1004aa03a34af7e2e877e527604972baaf3d0cb6
Sachin Karia <sachin.karia01@gmail.com>
2017-11-17 16:02:57 +0000

    completed working create review form

--- client/containers/ReviewForm.js
+++ client/containers/ReviewForm.js
@@ -15,19 +15,17 @@ class ReviewForm extends React.Component {
       food: null,
       service: null,
       hygiene: null,
-      overall: null
+      overall: null,
+      comment: null
     };
     this.baseState = this.state;
 
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
     this.resetForm = this.resetForm.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.onStarClick = this.onStarClick.bind(this);
-  }
-
-  hideModal() {
-    this.resetForm();
+    this.handleChange = this.handleChange.bind(this);
   }
 
   showModal() {
@@ -44,14 +42,23 @@ class ReviewForm extends React.Component {
     });
   }
 
-  handleFormSubmit() {
+  hideModal() {
+    this.resetForm();
+  }
+
+  handleChange(event) {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit() {
     this.props.createReview(this.state, this.props.id);
+    this.hideModal();
   }
 
   render() {
     const { user, auth } = this.props;
     const primaryColour = '#ff6851';
-    const emptyStarColor = '#cecccc';
+    const emptyStarColor = '#e4e2e2';
 
     return (
       <div>
@@ -68,11 +75,11 @@ class ReviewForm extends React.Component {
             <Modal.Title className="gc-profile-heading-md gc-center gc-margin-bottom">Leave a review</Modal.Title>
             <Row>
               <Col sm={8} smOffset={2}>
-                <p className="gc-center gc-text gc-text--grey">Please fill out the details of your event. This is just a request and you will not be charged until the booking is confirmed and you are invoiced.</p>
+                <p className="gc-center gc-text gc-text--grey">Please select the appropriate ratings for each category and add any additional comments.</p>
               </Col>
             </Row>
           </Modal.Header>
-          <Col sm={4} smOffset={4}>
+          <Col sm={8} smOffset={2}>
             <Modal.Body>
               {
                 (!auth.authenticated && !user.data) &&
@@ -83,79 +90,80 @@ class ReviewForm extends React.Component {
                 <Settings />
               }
               {(auth.authenticated && user.data && user.data.firstName && user.data.email && user.data.mobileNumber) &&
-              <form onSubmit={this.handleFormSubmit}>
               <div className="gc-center">
-                  <Row>
-                    <label className="gc-form-heading">Value</label>
-                    <br />
-                    <StarRatingComponent
-                      className="gc-star-rating"
-                      starColor={primaryColour}
-                      emptyStarColor={emptyStarColor}
-                      name="value"
-                      starCount={5}
-                      value={this.state.value}
-                      onStarClick={this.onStarClick}
-                    />
-                  </Row>
-                  <Row>
-                    <label className="gc-form-heading">Food/Drink</label>
-                    <br />
-                    <StarRatingComponent
-                      className="gc-star-rating"
-                      starColor={primaryColour}
-                      emptyStarColor={emptyStarColor}
-                      name="food"
-                      starCount={5}
-                      value={this.state.food}
-                      onStarClick={this.onStarClick}
-                    />
-                  </Row>
-                  <Row>
-                    <label className="gc-form-heading">Service</label>
-                    <br />
-                    <StarRatingComponent
-                      className="gc-star-rating"
-                      starColor={primaryColour}
-                      emptyStarColor={emptyStarColor}
-                      name="service"
-                      starCount={5}
-                      value={this.state.service}
-                      onStarClick={this.onStarClick}
-                    />
-                  </Row>
-                  <Row>
-                    <label className="gc-form-heading">Cleanliness</label>
-                    <br />
-                    <StarRatingComponent
-                      className="gc-star-rating"
-                      starColor={primaryColour}
-                      emptyStarColor={emptyStarColor}
-                      name="hygiene"
-                      starCount={5}
-                      value={this.state.hygiene}
-                      onStarClick={this.onStarClick}
-                    />
-                  </Row>
-                  <Row>
-                    <label className="gc-form-heading">Overall</label>
-                    <br />
-                    <StarRatingComponent
-                      className="gc-star-rating"
-                      starColor={primaryColour}
-                      emptyStarColor={emptyStarColor}
-                      name="overall"
-                      starCount={5}
-                      value={this.state.overall}
-                      onStarClick={this.onStarClick}
-                    />
-                  </Row>
-                  <Button block type="submit" className="gc-btn gc-btn--orange gc-margin-top">
-                    Submit review
-                  </Button>
-                </div>
-              </form>
-
+                <Row>
+                  <label className="gc-form-heading">Value</label>
+                  <br />
+                  <StarRatingComponent
+                    className="gc-star-rating"
+                    starColor={primaryColour}
+                    emptyStarColor={emptyStarColor}
+                    name="value"
+                    starCount={5}
+                    value={this.state.value}
+                    onStarClick={this.onStarClick}
+                  />
+                </Row>
+                <Row>
+                  <label className="gc-form-heading">Food/Drink</label>
+                  <br />
+                  <StarRatingComponent
+                    className="gc-star-rating"
+                    starColor={primaryColour}
+                    emptyStarColor={emptyStarColor}
+                    name="food"
+                    starCount={5}
+                    value={this.state.food}
+                    onStarClick={this.onStarClick}
+                  />
+                </Row>
+                <Row>
+                  <label className="gc-form-heading">Service</label>
+                  <br />
+                  <StarRatingComponent
+                    className="gc-star-rating"
+                    starColor={primaryColour}
+                    emptyStarColor={emptyStarColor}
+                    name="service"
+                    starCount={5}
+                    value={this.state.service}
+                    onStarClick={this.onStarClick}
+                  />
+                </Row>
+                <Row>
+                  <label className="gc-form-heading">Cleanliness</label>
+                  <br />
+                  <StarRatingComponent
+                    className="gc-star-rating"
+                    starColor={primaryColour}
+                    emptyStarColor={emptyStarColor}
+                    name="hygiene"
+                    starCount={5}
+                    value={this.state.hygiene}
+                    onStarClick={this.onStarClick}
+                  />
+                </Row>
+                <Row>
+                  <label className="gc-form-heading">Overall</label>
+                  <br />
+                  <StarRatingComponent
+                    className="gc-star-rating"
+                    starColor={primaryColour}
+                    emptyStarColor={emptyStarColor}
+                    name="overall"
+                    starCount={5}
+                    value={this.state.overall}
+                    onStarClick={this.onStarClick}
+                  />
+                </Row>
+                <Row>
+                  <br />
+                  <textarea onChange={this.handleChange} placeholder="Enter some useful information about your experience..." rows={4} className="gc-input form-control" name="comment" />
+                </Row>
+                <Button onClick={this.handleSubmit} block type="submit" className="gc-btn gc-btn--orange gc-margin-top">
+                  Submit review
+                </Button>
+              </div>
               }
             </Modal.Body>
           </Col>

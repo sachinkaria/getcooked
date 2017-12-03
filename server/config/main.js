cb8609d8da4e89b56a472932b1e0388611af6006
Sachin Karia <sachin.karia01@gmail.com>
2017-12-03 21:38:46 +0000

    split dev and prod keys into files

--- server/config/main.js
+++ server/config/main.js
@@ -1,18 +1,10 @@
 /**
  * Created by sachinkaria on 05/05/2017.
  */
-module.exports = {
-  // Secret key for JWT signing and encryption
-  jwt_secret: process.env.JWT_SECRET || '3456789pkjgfdfbnmgt7y89iuhgyr787iubh',
-  // Database connection information
-  database: process.env.DATABASE || 'mongodb://localhost/get-cooked',
-  // Server port
-  port: process.env.PORT || 3000,
-  twilio_account_sid: process.env.TWILIO_ACCOUNT_SID || 'AC1f9b1e64fada50b398d028d44653a48d',
-  twilio_token: process.env.TWILIO_TOKEN || '5d90b358c5c7b1e9f0ab8ed31b00f435',
-  aws_key: process.env.AWS_KEY || 'key',
-  aws_secret: process.env.AWS_SECRET || 'secret',
-  aws_bucket: process.env.AWS_BUCKET || 'getcooked',
-  aws_endpoint: process.env.AWS_ENDPOINT || 'localhost',
-  aws_port: process.env.AWS_PORT || '10001'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else if (process.env.NODE_ENV === 'test') {
+  module.exports = require('./test');
+} else {
+  module.exports = require('./dev');
+}

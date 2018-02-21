88c5eb584fcb39f6e9bc70d21354d73bf2da712e
Sachin Karia <sachin.karia01@gmail.com>
2018-02-21 19:19:56 +0000

    added conversion tracking

--- client/components/chefs/setup/payment.js
+++ client/components/chefs/setup/payment.js
@@ -42,6 +42,7 @@ class CheckoutForm extends React.Component {
   handleSubmit(ev) {
     ev.preventDefault();
     heap.track('Submit Payment Details');
+    gtag_report_conversion();
     const AUTH_HEADERS = {headers: {Authorization: localStorage.token}};
     axios.post('/api/stripe/customers', {
       email: localStorage.user.email,

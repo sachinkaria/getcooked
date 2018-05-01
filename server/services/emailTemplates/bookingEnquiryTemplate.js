aaa40c09de506387e8682e24ef6ab63bed419fb6
Sachin Karia <sachin.karia01@gmail.com>
2018-05-01 11:42:36 +0100

    update email booking template

--- server/services/emailTemplates/bookingEnquiryTemplate.js
+++ server/services/emailTemplates/bookingEnquiryTemplate.js
@@ -23,7 +23,7 @@ module.exports = (chef, user, booking, hostname) => {
           </li>
         </ul>
         <p>
-          You can access your bookings and confirm your availability via your dashboard at: ${hostname}. Once you confirm
+          You can access your bookings for more information and confirm your availability via your dashboard at: ${hostname}. Once you confirm
           your availability you will receive the contact details for the client.
         </p>
         <br>

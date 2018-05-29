aae0e97ccf5b94bc10a3887e194fb68c82be9ed6
Sachin Karia <sachin.karia01@gmail.com>
2018-05-29 10:50:44 +0100

    change window scroll arrow function to normal function

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import 'babel-polyfill';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers/index';
 import routes from './config/routes';
@@ -10,7 +11,6 @@ import { AUTH_USER } from './actions/types';
 import { getCurrentUser } from './actions/users';
 import { getBookings } from './actions/bookings';
 
-require('babel-polyfill');
 require('./styles/main.scss');
 require('./images/logo-icon.png');
 require('./images/chef.svg');
@@ -61,7 +61,7 @@ if (token) {
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory} routes={routes} />
+    <Router onUpdate={function(){window.scrollTo(0, 0)}} history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app'));
 

020a646a4adef6f3f79a995920915b1218640c6d
Sachin Karia <sachin.karia01@gmail.com>
2018-05-30 14:04:52 +0100

    changed compression code

--- server.js
+++ server.js
@@ -68,12 +68,13 @@ eventsRoutes(router);
 // Use our router configuration when we call /
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist'), router);
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-  });
   app.get('/*.js', (req, res) => {
     req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
   });
 } else {
   app.use('/', router);

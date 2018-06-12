d24b6ae46e118f1cf20cc76fce665c42d7b4051d
Sachin Karia <sachin.karia01@gmail.com>
2018-06-12 13:28:51 +0100

    added collapseable event panels to admin dashboard

--- client/components/admin/Dashboard/EventItem.js
+++ client/components/admin/Dashboard/EventItem.js
@@ -7,104 +7,113 @@ function EventItem({userItem, booking}) {
   return (
     <Row>
       <Col xs={12}>
-        <Panel>
-          <Panel.Heading className="gc-text gc-bold">Event Details</Panel.Heading>
-          <Panel.Body>
-            <Row>
-              <Col xs={12}>
-                <p className="gc-text gc-grey">Contact Details</p>
-                <p className="gc-text">
-                  {userItem.firstName} {userItem.lastName}
-                </p>
-                <p className="gc-text">
-                  {userItem.email}
-                </p>
-                <p className="gc-text">
-                  {userItem.mobileNumber}
-                </p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Date</p>
-                <p className="gc-text">{moment(booking.date).format('MMMM Do YYYY')}</p>
-                <hr className="visible-xs" />
-              </Col>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Event Address</p>
-                <p className="gc-text gc-margin-none">
-                  {booking.address.line1} {booking.address.line2},
-                </p>
-                <p className="gc-text">
-                  {booking.address.postcode}
-                </p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Event Type</p>
-                <p className="gc-text text-capitalize">{booking.eventType}</p>
-              </Col>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Number of guests</p>
-                <p className="gc-text">{booking.numberOfPeople} people</p>
-              </Col>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Budget</p>
-                <p className="gc-text">£{booking.budget || 1500}</p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Services Required</p>
-                <div className="gc-margin-bottom">
-                  {
-                    booking.services.map((service) => {
-                      return (
-                        <p key={service} className="text-capitalize gc-margin-none">
-                          {service}
-                        </p>
-                      );
-                    })
-                  }
-                </div>
-              </Col>
-              <Col xs={12} sm={4}>
-                <p className="gc-text gc-grey">Type of Food</p>
-                <div className="gc-margin-bottom">
-                  {
-                    booking.foodServices.map((service) => {
-                      return (
-                        <p key={service} className="text-capitalize gc-margin-none">
-                          {service}
-                        </p>
-                      );
-                    })
-                  }
-                </div>
-              </Col>
-              <Col xs={12} sm={4}>
-                <Link to={'/admin/dashboard/chefs'}>
-                  <Button onClick={() => sessionStorage.setItem('event', JSON.stringify(booking))}>Send to Caterers</Button>
-                </Link>
-              </Col>
-            </Row>
-            {
-              booking.additionalInformation &&
+        <Panel id="collapsible-panel-example-2">
+          <Panel.Heading>
+            <Panel.Title toggle>
+              <p className="gc-text pull-left">
+                {userItem.firstName} {userItem.lastName}
+              </p>
+              <p className="gc-text text-right">{moment(booking.updatedAt).format('MMMM Do YYYY')}</p>
+            </Panel.Title>
+          </Panel.Heading>
+          <Panel.Collapse>
+            <Panel.Body>
               <Row>
                 <Col xs={12}>
-                  <hr />
-                  <p className="gc-text gc-grey">Additional Information</p>
-                  <p className="gc-text gc-dark-grey">
-                    {booking.additionalInformation}
+                  <p className="gc-text gc-grey">Contact Details</p>
+                  <p className="gc-text">
+                    {userItem.firstName} {userItem.lastName}
+                  </p>
+                  <p className="gc-text">
+                    {userItem.email}
+                  </p>
+                  <p className="gc-text">
+                    {userItem.mobileNumber}
                   </p>
                 </Col>
               </Row>
-            }
-          </Panel.Body>
+              <hr />
+              <Row>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Date</p>
+                  <p className="gc-text">{moment(booking.date).format('MMMM Do YYYY')}</p>
+                  <hr className="visible-xs" />
+                </Col>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Event Address</p>
+                  <p className="gc-text gc-margin-none">
+                    {booking.address.line1} {booking.address.line2},
+                  </p>
+                  <p className="gc-text">
+                    {booking.address.postcode}
+                  </p>
+                </Col>
+              </Row>
+              <hr />
+              <Row>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Event Type</p>
+                  <p className="gc-text text-capitalize">{booking.eventType}</p>
+                </Col>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Number of guests</p>
+                  <p className="gc-text">{booking.numberOfPeople} people</p>
+                </Col>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Budget</p>
+                  <p className="gc-text">£{booking.budget || 1500}</p>
+                </Col>
+              </Row>
+              <hr />
+              <Row>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Services Required</p>
+                  <div className="gc-margin-bottom">
+                    {
+                      booking.services.map((service) => {
+                        return (
+                          <p key={service} className="text-capitalize gc-margin-none">
+                            {service}
+                          </p>
+                        );
+                      })
+                    }
+                  </div>
+                </Col>
+                <Col xs={12} sm={4}>
+                  <p className="gc-text gc-grey">Type of Food</p>
+                  <div className="gc-margin-bottom">
+                    {
+                      booking.foodServices.map((service) => {
+                        return (
+                          <p key={service} className="text-capitalize gc-margin-none">
+                            {service}
+                          </p>
+                        );
+                      })
+                    }
+                  </div>
+                </Col>
+                <Col xs={12} sm={4}>
+                  <Link to={'/admin/dashboard/chefs'}>
+                    <Button onClick={() => sessionStorage.setItem('event', JSON.stringify(booking))}>Send to Caterers</Button>
+                  </Link>
+                </Col>
+              </Row>
+              {
+                booking.additionalInformation &&
+                <Row>
+                  <Col xs={12}>
+                    <hr />
+                    <p className="gc-text gc-grey">Additional Information</p>
+                    <p className="gc-text gc-dark-grey">
+                      {booking.additionalInformation}
+                    </p>
+                  </Col>
+                </Row>
+              }
+            </Panel.Body>
+          </Panel.Collapse>
         </Panel>
       </Col>
     </Row>

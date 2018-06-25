cc5e54ca50941e83d9e88ca258dcb7fb9687056b
Sachin Karia <sachin.karia01@gmail.com>
2018-06-25 15:21:46 +0100

    updated footer blog link

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -39,7 +39,7 @@ export default function Footer() {
                 <a className="gc-link-default gc-link-default--white" href="mailto:team@getcooked.co?Subject=Website%20Enquiry" target="_top">Contact Us</a>
               </ul>
               <ul className="gc-text gc-white gc-padding-none">
-                Blog (coming soon)
+                <Link className="gc-link-default gc-link-default--white" to="/blog">Blog</Link>
               </ul>
             </li>
           </Col>

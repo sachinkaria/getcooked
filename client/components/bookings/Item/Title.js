15faf96830ee6fbc94490cdab49845b0df3b956c
Sachin Karia <sachin.karia01@gmail.com>
2018-07-27 15:30:02 +0100

    final touches on new booking form

--- client/components/bookings/Item/Title.js
+++ client/components/bookings/Item/Title.js
@@ -2,12 +2,13 @@
  * Created by sachinkaria on 24/07/2018.
  */
 import React from 'react';
+import _ from 'lodash';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
 const Title = props => (
   <h1 className="gc-section-heading-sm gc-margin-none">
-    {props.name}&#39;s event on {moment(props.date).format('MMMM Do YYYY')}
+    {_.capitalize(props.name)}&#39;s event on {moment(props.date).format('MMMM Do YYYY')}
   </h1>
 );
 
--- client/containers/forms/booking/BudgetAdditionalInformationForm.js
+++ client/containers/forms/booking/BudgetAdditionalInformationForm.js
@@ -54,7 +54,7 @@ class DateAndPlaceForm extends Component {
       <div>
         <form onSubmit={handleSubmit(this.handleFormSubmit)}>
           <div>
-            <label className="gc-text">Approximately many guests are you expecting?</label>
+            <label className="gc-text gc-text--lg gc-text--slim">Approximately many guests are you expecting?</label>
             <div className="gc-margin-bottom">
               <Field
                 name="numberOfPeople"
@@ -64,7 +64,7 @@ class DateAndPlaceForm extends Component {
                 type="number"
               />
             </div>
-            <label className="gc-text">What is estimated budget for the entire event? (incl. staff, equipment & food)</label>
+            <label className="gc-text gc-text--lg gc-text--slim">What is estimated budget for the entire event? (incl. staff, equipment & food)</label>
             <div className="gc-margin-bottom">
               <Field
                 addonText="£"
@@ -76,7 +76,7 @@ class DateAndPlaceForm extends Component {
                 type="number"
               />
             </div>
-            <label className="gc-text">Do you have any additional information that you would like to provide us with?</label>
+            <label className="gc-text gc-text--lg gc-text--slim">Do you have any additional information that you would like to provide us with?</label>
             <div className="gc-margin-bottom">
               <Field
                 name="additionalInformation"

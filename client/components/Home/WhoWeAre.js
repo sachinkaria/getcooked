a5070689a535845a9e68607c64ab327440b20830
Sachin Karia <sachin.karia01@gmail.com>
2018-08-29 15:58:06 +0100

    updated home copy

--- client/components/Home/WhoWeAre.js
+++ client/components/Home/WhoWeAre.js
@@ -17,8 +17,8 @@ function WhoWeAre() {
             <Col sm={6} smPull={6}>
               <h3 className="gc-section-heading gc-padding-none">Who we are</h3>
               <p className="gc-text gc-text--lg">
-                Get Cooked gives you the opportunity to book professional caterers for events organised by you.
-                Compare and book the best professional, sustainable and bespoke caterers around. Book caterers for your
+                Get Cooked gives you the opportunity to book professional caterers and private chefs for events organised by you.
+                Compare and book the best professional, sustainable and bespoke caterering companies and chefs around. Cater your
                 weddings, parties, corporate events, private dinners and more.
               </p>
             </Col>

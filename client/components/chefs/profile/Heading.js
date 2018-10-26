f232cc45fac5497052d8a167655168a406ddc9cb
Sachin Karia <sachin.karia01@gmail.com>
2018-10-26 19:36:07 +0100

    added location to profile

--- client/components/chefs/profile/Heading.js
+++ client/components/chefs/profile/Heading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Heading = (props) => {
-  const classes = classNames('gc-profile-heading-lg gc-margin-top--xs gc-margin-bottom text-capitalize', {
+  const classes = classNames('gc-profile-heading-lg gc-margin-top--xs text-capitalize', {
     'gc-center': !props.textAlign
   });
   return <h1 className={classes}>{props.text}</h1>;
